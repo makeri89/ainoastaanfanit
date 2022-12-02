4390022c6fd059ebef2c207cbfe7ff44a298b503
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-12-02 17:31:03 +0200

    Added info on image adder

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -8,15 +8,17 @@ import {
   ModalCloseButton,
   Image,
   Heading,
+  Text,
 } from '@chakra-ui/react'
 
 interface Props {
-  image?: { src: string; name: string }
+  image?: { src: string; name: string; user: string }
   isOpen: boolean
   onClose: () => void
 }
 
 const ImageModal = ({ image, isOpen, onClose }: Props) => {
+  console.log(image)
   return (
     <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick size="xl">
       <ModalOverlay />
@@ -29,6 +31,7 @@ const ImageModal = ({ image, isOpen, onClose }: Props) => {
           <Box margin="10px">
             <Image src={image?.src} alt="Tractor" />
           </Box>
+          <Text color="black">Lisääjä: {image?.user}</Text>
         </ModalBody>
       </ModalContent>
     </Modal>
