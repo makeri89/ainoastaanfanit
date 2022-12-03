ed3ff79180d50693a4da5b3267f81d35cfd21f0b
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-12-03 19:13:39 +0200

    Adde metadata and custom favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,35 @@ import { Analytics } from '@vercel/analytics/react'
 import '@fontsource/comic-neue'
 import { SessionProvider } from 'next-auth/react'
 import Wrapper from '../components/Wrapper'
+import Head from 'next/head'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider>
+      <Head>
+        <title>Ainoastaan fanit</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#367c2b" />
+        <meta name="msapplication-TileColor" content="#367c2b" />
+        <meta name="theme-color" content="#367c2b" />
+      </Head>
       <ChakraProvider theme={theme}>
         <Wrapper>
           <Component {...pageProps} />
