e6b536a67b19603890ae4cafa7230054c7a823f8
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-12-03 20:26:23 +0200

    Added link to content policy on profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading } from '@chakra-ui/react'
+import { Button, Flex, Heading } from '@chakra-ui/react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import LoginButton from '../components/LoginButton'
@@ -12,12 +12,21 @@ const Profile = () => {
     router.push('/')
   }
 
+  const handleContentPolicy = () => {
+    router.push('/contentpolicy')
+  }
+
   return (
     <>
       <Heading>Profiili</Heading>
-      <Button variant="outline" onClick={handleFrontPage}>
-        Etusivu
-      </Button>
+      <Flex gap="10px">
+        <Button variant="outline" onClick={handleFrontPage}>
+          Etusivu
+        </Button>
+        <Button variant="outline" onClick={handleContentPolicy}>
+          Sisällön säännöt
+        </Button>
+      </Flex>
       {status === 'authenticated' ? (
         <>
           <UserInfo user={session?.user} />
